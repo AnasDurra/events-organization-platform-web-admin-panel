4b704bbd018883ad03145672a196651633574d37
gabsonia_ANAS <anas.reish.77@gmail.com>
2024-08-20 23:33:32 +0300

    rewards cards retouch

--- src/features/gamification/rules/BadgeRewardCard.jsx
+++ src/features/gamification/rules/BadgeRewardCard.jsx
@@ -11,20 +11,21 @@ const niceColors = ['#fbaf51', '#ce355f', '#474f7f', '#e7a99d', '#a986af'];
 export default function BadgeRewardCard({ onDelete, badge }) {
     return (
         <Card
-            className='shadow-lg hover:shadow-sm'
+            className='shadow-lg hover:shadow-sm relative '
+            styles={{ body: { padding: '0.2rem' } }}
             //title={<div className='text-center text-white'>{badge?.reward?.name}</div>}
-
+            style={{ borderColor: '#ce355f', borderWidth: '15px 0 15px 0', height: 'auto' }}
             onClick={() => {}}
             bordered
         >
-            <div className='flex justify-end items-center mb-[-2em] '>
+            <div className='absolute top-2 right-2'>
                 <CloseOutlined
-                    className='text-[1.5em] text-gray-500 hover:cursor-pointer'
+                    className='text-[1.5em] text-gray-500 hover:text-red-600 hover:cursor-pointer'
                     onClick={onDelete}
                 />
             </div>
             <div className='flex justify-center items-center'>
-                <div className='text-lg font-bold bg-gradient-to-r from-[#fbaf51] via-[#ce355f] to-[#a986af] inline-block text-transparent bg-clip-text'>
+                <div className='text-lg font-extrabold uppercase tracking-wider  bg-gradient-to-r from-[#fbaf51] via-[#ce355f] to-[#a986af] inline-block text-transparent bg-clip-text'>
                     {badge?.title}
                 </div>
             </div>
@@ -33,17 +34,17 @@ export default function BadgeRewardCard({ onDelete, badge }) {
                 {badge?.shape?.svg && (
                     <img
                         src={`data:image/svg+xml;utf8,${encodeURIComponent(badge.shape.svg)}`}
-                        className='w-[9.5em]'
+                        className='w-[10.5em]'
                     ></img>
                 )}
             </div>
-            <div className='flex justify-end items-end space-x-1 text-primary my-2 mt-[-1.2em] mr-[-0.5em]  '>
-                {badge?.anonymous && <AiOutlineEyeInvisible className='text-[1.4em] text-red-400' />}
+            <div className='flex justify-end items-end space-x-1 relative right-0 bottom-0  '>
+                {badge?.anonymous && <AiOutlineEyeInvisible className='text-[1.8rem] text-red-400' />}
 
-                {badge?.anonymous != true && <AiOutlineEye className='text-[1.4em] text-green-400' />}
+                {badge?.anonymous != true && <AiOutlineEye className='text-[1.8rem] text-green-400' />}
 
-                {badge?.visibility != true && <GoShieldSlash className='text-[1.4em] text-red-400' />}
-                {badge?.visibility == true && <IoShieldCheckmarkOutline className='text-[1.4em] text-green-400' />}
+                {badge?.visibility != true && <GoShieldSlash className='text-[1.8rem] text-red-400' />}
+                {badge?.visibility == true && <IoShieldCheckmarkOutline className='text-[1.8rem] text-green-400' />}
             </div>
         </Card>
     );
--- src/features/gamification/rules/PointsRewardCard.jsx
+++ src/features/gamification/rules/PointsRewardCard.jsx
@@ -1,54 +1,66 @@
 import { CloseOutlined } from '@ant-design/icons';
-import { CancelOutlined } from '@mui/icons-material';
-import { Card, Divider } from 'antd';
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { Card, Divider } from 'antd';
 
 export default function PointsRewardCard({ value, name, type, onDelete }) {
     return (
         <Card
             key={uuidv4()}
-            onClick={() => {}}
             bordered
-            className='shadow-lg hover:shadow-none '
+            className='relative  bg-white rounded-lg shadow-lg  hover:shadow-xl transition-transform duration-300 ease-out bg-gradient-to-br   '
+            style={{ borderColor: type === 'pp' ? '#34D399' : '#FBBF24', borderWidth: '15px 0 15px 0', height: 'auto' }}
+            styles={{ body: { padding: '0.2rem' } }}
         >
-            <div className='flex justify-end items-center mb-[-2em] '>
+            <div className='absolute top-2 right-2'>
                 <CloseOutlined
-                    className='text-[1.5em] text-gray-500 hover:cursor-pointer'
+                    className='text-[1.5em] text-gray-500 hover:text-red-600 hover:cursor-pointer'
                     onClick={onDelete}
                 />
             </div>
-            <div className='mt-[-2em] flex flex-col justify-center items-center'>
+
+            <div className='flex flex-col justify-center items-center mt-0'>
                 <div
-                    className={`text-lg font-bold bg-gradient-to-r  text-transparent bg-clip-text inline-block ${
-                        type == 'pp'
-                            ? 'from-green-500 via-green-600 to-green-900'
-                            : type == 'rp'
-                            ? 'from-yellow-500 via-yellow-600 to-yellow-900'
-                            : null
+                    className={`text-lg font-extrabold uppercase tracking-wider bg-gradient-to-r text-transparent bg-clip-text ${
+                        type === 'pp'
+                            ? 'from-green-400 via-green-500 to-green-600'
+                            : type === 'rp'
+                            ? 'from-yellow-400 via-yellow-500 to-yellow-600'
+                            : ''
                     }`}
                 >
                     {name}
                 </div>
-                <div className='flex justify-center items-center flex-col'>
+
+                <div className='flex justify-center items-center flex-col mt-3'>
                     <img
                         src={
-                            type == 'pp'
+                            type === 'pp'
                                 ? `/static/images/game-point.svg`
-                                : type == 'rp'
+                                : type === 'rp'
                                 ? `/static/images/points-rp.svg`
                                 : null
                         }
-                        className='w-[7em]'
-                    ></img>
-                    <Divider
-                        style={{ margin: 0 }}
-                        className='font-mono'
+                        className='w-[6rem] mb-0'
+                        alt='reward-icon'
+                    />
+                    <div
+                        className={`text-xl font-extrabold uppercase tracking-wider bg-gradient-to-r text-transparent bg-clip-text ${
+                            type === 'pp'
+                                ? 'from-green-400 via-green-500 to-green-600'
+                                : type === 'rp'
+                                ? 'from-yellow-400 via-yellow-500 to-yellow-600'
+                                : ''
+                        }`}
                     >
                         {value}
-                    </Divider>
+                    </div>
                 </div>
             </div>
+
+            {/*  <div className='absolute bottom-2 left-2 text-xs text-gray-500 font-semibold'>
+                {type === 'pp' ? 'Platform Points' : type === 'rp' ? 'Redeemable Points' : ''}
+            </div> */}
         </Card>
     );
 }
